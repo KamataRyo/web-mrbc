d0bc43bb9a73ea30746226b7e1d2db1bece05cd5
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-07-22 08:43:22 +0900

    Initiate server code

--- server/bin/install.sh
+++ server/bin/install.sh
@@ -2,13 +2,17 @@
 set -eu
 
 # build mrbc with bytecode format version.3
+if [[ -d "mruby" ]]; then
+    rm -rf mruby
+fi
 git clone --depth=1 https://github.com/mruby/mruby.git
 cd mruby && make
 cd ..
-ln -s mruby/bin/mrbc ./bin/mrbc3
 
 # build mrbc with bytecode format version.2
+if [[ -d "mrbc" ]]; then
+    rm -rf mrbc
+fi
 git clone --depth=1 git@bitbucket.org:tarosay/mrbc.git
 cd mrbc && make
 cd ..
-ln -s mrubc/mrbc ./bin/mrbc2
